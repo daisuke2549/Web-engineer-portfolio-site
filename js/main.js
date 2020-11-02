a5a6bf89df4414086f42e25aa9b11fc0db9283a0
daisuke sasaki <gerund25495641@gmail.com>
2020-11-03 08:12:50 +0900

    jQueryのハンバーガメニューまで実装まで完了

--- js/main.js
+++ js/main.js
@@ -8,3 +8,68 @@ $('#toggle').click(function() {
        $("html div").not("#overlay").not(".button_container").addClass("blur")
     }
  });
+ 
+ $('.menu_aboutme').click(function() {
+   $("html,body").animate({scrollTop:$(".profile_title").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+ $('.menu_introduction').click(function() {
+   $("html,body").animate({scrollTop:$("#introduction_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+ $('.menu_backgrounds').click(function() {
+   $("html,body").animate({scrollTop:$("#backgrounds_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+
+ $('.menu_skills').click(function() {
+   $("html,body").animate({scrollTop:$("#skills_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+
+ $('.menu_services').click(function() {
+   $("html,body").animate({scrollTop:$("#service_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+
+
+ $('.menu_my_works').click(function() {
+   $("html,body").animate({scrollTop:$("#my_works_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+
+ $('.menu_contact').click(function() {
+   $("html,body").animate({scrollTop:$("#contact_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+
+ $('.menu_sns').click(function() {
+   $("html,body").animate({scrollTop:$("#sns_section").offset().top});
+   $('#overlay').removeClass("open");
+   $('html div').removeClass("blur");
+   $("#toggle").toggleClass("active");
+ });
+
+
+ $(function () {
+   $('.myname').textillate();
+ })
+ $(function () {
+   $('.worker_type').textillate();
+ })
+
+ 
